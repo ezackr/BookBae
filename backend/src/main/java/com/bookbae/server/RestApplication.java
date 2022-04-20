3ce8a12790ca30c1c66056f699c370ee80ad88a4
Joshua Peterson <jpete4@uw.edu>
2022-04-19 23:07:52 -0700

    Lay the groundwork for a connection pool

--- backend/src/main/java/com/bookbae/server/RestApplication.java
+++ backend/src/main/java/com/bookbae/server/RestApplication.java
@@ -2,7 +2,34 @@ package com.bookbae.server;
 
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
+import jakarta.inject.Singleton;
+import org.apache.commons.dbcp2.BasicDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
+@Singleton
 @ApplicationPath("/v1")
 public class RestApplication extends Application {
+
+    private BasicDataSource dataSource;
+
+    public RestApplication() {
+        try {
+            Class.forName("org.h2.Driver");
+        } catch (ClassNotFoundException e) {} //TODO: replace this with a real driver
+        
+        this.dataSource = new BasicDataSource();
+        this.dataSource.setUrl("jdbc:h2:mem:db1");
+        // could initialize the testing db here
+        // but in a real-world situation would be loading a pre-configured database
+        //TODO: find out how to get runtime configuration info to do tests
+    }
+
+    public String getString() {
+        return "never gonna give u up";
+    }
+
+    public Connection getConnection() throws SQLException {
+        return this.dataSource.getConnection();
+    }
 }
--- backend/src/main/java/com/bookbae/server/UserResource.java
+++ backend/src/main/java/com/bookbae/server/UserResource.java
@@ -3,9 +3,16 @@ package com.bookbae.server;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.GET;
+import java.sql.Connection;
+import java.sql.SQLException;
+import jakarta.inject.Inject;
 
 @Path("/user")
 public class UserResource {
+    
+    @Inject
+    private RestApplication application;
+
     @GET @Produces("text/plain")
     public String helloWorld() {
         return "hi!";
@@ -27,5 +34,26 @@ public class UserResource {
     @GET @Path("/matches")
     public String getMatches() {
         return "{}";
+    }
+
+    @Produces("application/json")
+    @GET @Path("/application")
+    public String getApplication() {
+        return "{ \"application\": \"" + this.application.getString() + "\", " + "\"id\": \"" + this.application.toString() + "\"}";
+    }
+
+    @Produces("application/json")
+    @GET @Path("/database")
+    public String getDatabase() {
+        String str;
+        try {
+            Connection conn = this.application.getConnection();
+            //TODO: SQL stuff
+            conn.close();
+            str = "{\"connection\": \"" + conn.toString() + "\"}";
+        } catch (SQLException e) {
+            str = "{\"connection\": \"none\", \"error\": \"" + e.toString() + "\"}";
+        }
+        return str;
     }
 }
