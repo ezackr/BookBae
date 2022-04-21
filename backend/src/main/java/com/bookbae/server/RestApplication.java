0fe2e939c2942de156353ddafa7c47fad4fcdc5a
Joshua Peterson <jpete4@uw.edu>
2022-04-21 16:04:39 -0700

    Tweak to database connection creation

--- backend/src/main/java/com/bookbae/server/RestApplication.java
+++ backend/src/main/java/com/bookbae/server/RestApplication.java
@@ -16,7 +16,8 @@ public class RestApplication extends Application {
     public RestApplication() {
         this.dataSource = new BasicDataSource();
         this.dataSource.setUrl("jdbc:h2:mem:db1");
-        this.dataSource.setDriverClassName("org.h2.Driver"); //testme`
+        this.dataSource.setDriverClassName("org.h2.Driver");
+        this.dataSource.setPoolPreparedStatements(true);
         // could initialize the testing db here
         // but in a real-world situation would be loading a pre-configured database
         //TODO: find out how to get runtime configuration info to do tests
