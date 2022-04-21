c6fa8688a7357ba7db3ddfac34bf847b78a545dc
Joshua Peterson <jpete4@uw.edu>
2022-04-21 13:51:57 -0700

    Load the driver properly rather than doing some weird stuff

--- backend/src/main/java/com/bookbae/server/RestApplication.java
+++ backend/src/main/java/com/bookbae/server/RestApplication.java
@@ -14,12 +14,9 @@ public class RestApplication extends Application {
     private BasicDataSource dataSource;
 
     public RestApplication() {
-        try {
-            Class.forName("org.h2.Driver");
-        } catch (ClassNotFoundException e) {} //TODO: replace this with a real driver
-        
         this.dataSource = new BasicDataSource();
         this.dataSource.setUrl("jdbc:h2:mem:db1");
+        this.dataSource.setDriverClassName("org.h2.Driver"); //testme`
         // could initialize the testing db here
         // but in a real-world situation would be loading a pre-configured database
         //TODO: find out how to get runtime configuration info to do tests
