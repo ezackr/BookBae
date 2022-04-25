1dd9ba42b025358ef287049fdc83c7c625e2defe
Joshua Peterson <jpete4@uw.edu>
2022-04-25 13:19:46 -0700

    Fix ordering of tests, make build.gradle neater

--- backend/src/test/java/IntegrationTest.java
+++ backend/src/test/java/IntegrationTest.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumeFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -8,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class IntegrationTest {
     @Test
     void testIntegratedLogin() {
-        assumeTrue(System.getProperty("bookbae.testServer") != null);
         assumeFalse(System.getProperty("bookbae.testServer").equals(""));
-        assertEquals(System.getProperty("bookbae.testServer"), "http://mint:8080/api");
+        assertEquals("http://localhost:8080/api", System.getProperty("bookbae.testServer"));
+        //TODO: write real integration tests that make calls against the server
     }
 }
