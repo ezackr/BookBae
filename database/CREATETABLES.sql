28269b616d5835f9b355283415b28b56110ff71a
Eado51415 <eado@cs.washington.edu>
2022-04-25 15:02:43 -0700

    Edit CREATETABLES.sql to align better with chosen SQL coding guidelines

--- database/CREATETABLES.sql
+++ database/CREATETABLES.sql
@@ -1,6 +1,6 @@
 CREATE TABLE user_info
 (
-    id UNIQUEIDENTIFIER NOT NULL,
+    user_id UNIQUEIDENTIFIER NOT NULL,
     name VARCHAR(64) NOT NULL,
     gender VARCHAR(2) NOT NULL,
     phone_num VARCHAR(15) NOT NULL,
@@ -9,19 +9,19 @@ CREATE TABLE user_info
     email VARCHAR(254) NOT NULL,
     zipcode CHAR(5) NOT NULL,
     bio VARCHAR(500) NOT NULL,
-    PRIMARY KEY (id),
+    PRIMARY KEY (user_id),
     UNIQUE (phone_num),
     UNIQUE (email)
 );
 
 CREATE TABLE prompt
 (
-    id TINYINT NOT NULL,
+    prompt_id TINYINT NOT NULL,
     question VARCHAR(100) NOT NULL,
     answer VARCHAR(250) NOT NULL,
     user_id UNIQUEIDENTIFIER NOT NULL,
-    PRIMARY KEY (id, user_id),
-    FOREIGN KEY (user_id) REFERENCES user_info(id)
+    PRIMARY KEY (prompt_id, user_id),
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE preference
@@ -31,21 +31,21 @@ CREATE TABLE preference
     within_x_miles SMALLINT NOT NULL,
     user_id UNIQUEIDENTIFIER NOT NULL,
     PRIMARY KEY (user_id),
-    FOREIGN KEY (user_id) REFERENCES user_info(id)
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE target_gender
 (
     gender CHAR(2) NOT NULL,
     user_id UNIQUEIDENTIFIER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user_info(id)
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE user_book
 (
     book_id VARCHAR(12) NOT NULL,
     user_id UNIQUEIDENTIFIER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user_info(id)
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE login_info
@@ -55,7 +55,7 @@ CREATE TABLE login_info
     hash TEXT NOT NULL,
     user_id UNIQUEIDENTIFIER NOT NULL,
     PRIMARY KEY (user_id),
-    FOREIGN KEY (user_id) REFERENCES user_info(id)
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE likes
@@ -64,26 +64,26 @@ CREATE TABLE likes
     liker_user_id UNIQUEIDENTIFIER NOT NULL,
     liked_user_id UNIQUEIDENTIFIER NOT NULL,
     PRIMARY KEY (liker_user_id, liked_user_id),
-    FOREIGN KEY (liker_user_id) REFERENCES user_info(id),
-    FOREIGN KEY (liked_user_id) REFERENCES user_info(id)
+    FOREIGN KEY (liker_user_id) REFERENCES user_info(user_id),
+    FOREIGN KEY (liked_user_id) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE chat
 (
-    id UNIQUEIDENTIFIER NOT NULL,
+    chat_id UNIQUEIDENTIFIER NOT NULL,
     user_id1 UNIQUEIDENTIFIER NOT NULL,
     user_id2 UNIQUEIDENTIFIER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id1) REFERENCES user_info(id),
-    FOREIGN KEY (user_id2) REFERENCES user_info(id)
+    PRIMARY KEY (chat_id),
+    FOREIGN KEY (user_id1) REFERENCES user_info(user_id),
+    FOREIGN KEY (user_id2) REFERENCES user_info(user_id)
 );
 
 CREATE TABLE chat_line
 (
-    id INT NOT NULL,
+    line_id INT NOT NULL,
     line_text TEXT NOT NULL,
     timestamp TIMESTAMP NOT NULL,
     chat_id UNIQUEIDENTIFIER NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (chat_id) REFERENCES chat(id)
+    PRIMARY KEY (line_id, chat_id),
+    FOREIGN KEY (chat_id) REFERENCES chat(chat_id)
 );
