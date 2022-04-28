501598beb08b65971880349ec41a48577ce3d4ca
Joshua Peterson <jpete4@uw.edu>
2022-04-28 15:39:00 -0700

    Use Jersey so tests don't fail with ClassNotFound

--- backend/src/test/java/UserTest.java
+++ backend/src/test/java/UserTest.java
@@ -2,6 +2,9 @@ import org.junit.jupiter.api.Test;
 
 import com.bookbae.server.RestApplication;
 import com.bookbae.server.User;
+import com.bookbae.server.json.UserResponse;
+import jakarta.ws.rs.core.SecurityContext;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -10,6 +13,10 @@ public class UserTest {
     void getUserTest() {
         RestApplication application = new MockRestApplication();
         User resource = new User(application);
-        assertEquals("{}", "{}");
+        UUID userid = UUID.randomUUID();
+        SecurityContext ctx = new MockSecurityContext(userid.toString());
+        var resp = resource.getUser(ctx);
+        UserResponse entity = (UserResponse) resp.getEntity();
+        assertEquals(userid.toString(), entity.getUserId());
     }
 }
