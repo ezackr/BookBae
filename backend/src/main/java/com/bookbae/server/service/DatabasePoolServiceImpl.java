2da3b8d4b5be284756b6746514518fddf717d0d4
Eado51415 <eado@cs.washington.edu>
2022-05-02 23:28:47 -0700

    Add functionality to connect with dbconns.properties instead of string literal

--- backend/src/main/java/com/bookbae/server/service/DatabasePoolServiceImpl.java
+++ backend/src/main/java/com/bookbae/server/service/DatabasePoolServiceImpl.java
@@ -5,6 +5,10 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import jakarta.enterprise.context.ApplicationScoped;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @ApplicationScoped
 public class DatabasePoolServiceImpl implements DatabasePoolService {
@@ -12,13 +16,37 @@ public class DatabasePoolServiceImpl implements DatabasePoolService {
 
     public DatabasePoolServiceImpl() {
         this.dataSource = new BasicDataSource();
-        this.dataSource.setUrl("jdbc:h2:mem:db1");
-        this.dataSource.setDriverClassName("org.h2.Driver");
+        this.dataSource.setUrl(getConnectionUrl());
+        this.dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         this.dataSource.setPoolPreparedStatements(true);
     }
 
     @Override
     public Connection getConnection() throws SQLException {
         return this.dataSource.getConnection();
+    }
+
+    private String getConnectionUrl() {
+
+        // grab database credentials from dbconn.properties file
+        Properties configProps = new Properties();
+        try {
+            configProps.load(new FileInputStream("dbconn.properties"));
+        } catch(FileNotFoundException e) {
+            System.out.println(e);
+        } catch(IOException e) {
+            System.out.println(e);
+        }
+
+        String serverURL = configProps.getProperty("bookbae.server_url");
+        String dbName = configProps.getProperty("bookbae.database_name");
+        String adminName = configProps.getProperty("bookbae.username");
+        String password = configProps.getProperty("bookbae.password");
+        String connectionUrl =
+                String.format("jdbc:sqlserver://%s:1433;database=%s;user=%s;password=%s;encrypt=true;" +
+                                "trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;" +
+                                "loginTimeout=30;",
+                        serverURL, dbName, adminName, password);
+        return connectionUrl;
     }
 }
