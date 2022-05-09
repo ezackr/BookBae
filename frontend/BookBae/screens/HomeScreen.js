67e4dcd2e350b8da8380017fde8debeadc6c7435
Elliott Zackrone <ezackr@cs.washington.edu>
2022-05-09 01:31:29 -0700

    added default outlines of screens

--- frontend/BookBae/screens/HomeScreen.js
+++ frontend/BookBae/screens/HomeScreen.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import {StyleSheet, Text, SafeAreaView} from 'react-native';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import MainScreen from './MainScreen';
-import MessageScreen from './MessageScreen';
-import ProfileScreen from './ProfileScreen';
-
-const Tab = createMaterialTopTabNavigator();
 
 const HomeScreen = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Tab.Navigator>
-        <Tab.Screen name="MainScreen" component={MainScreen} />
-        <Tab.Screen name="MessageScreen" component={MessageScreen} />
-        <Tab.Screen name="ProfileScreen" component={ProfileScreen} />
-      </Tab.Navigator>
+      <Text>Welcome to BookBae!</Text>
     </SafeAreaView>
   );
 };
