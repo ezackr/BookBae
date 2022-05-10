14b2048cab26b0af672170d6aca22b284a2247c5
Elliott Zackrone <ezackr@cs.washington.edu>
2022-05-10 16:03:15 -0700

    added visible sections of swiping screen

--- frontend/BookBae/screens/HomeScreen.js
+++ frontend/BookBae/screens/HomeScreen.js
@@ -1,20 +1,51 @@
 import React from 'react';
-import {StyleSheet, Text, SafeAreaView, Pressable} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  Pressable,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
 
 const HomeScreen = ({navigation}) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Welcome to BookBae!</Text>
-      <Pressable
-        style={styles.button}
-        onPress={navigation.navigate('MessagesScreen')}>
-        <Text style={styles.buttonText}>Create Account</Text>
-      </Pressable>
+      <SafeAreaView style={styles.topMenu}>
+        <Text style={styles.title}>BookBae</Text>
+      </SafeAreaView>
+      <SafeAreaView style={styles.matchMenu}>
+        <ProfileOverview />
+      </SafeAreaView>
+      <SafeAreaView style={styles.bottomMenu}>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source="../images/deny.png"
+            style={styles.button}
+            resizeMode="stretch"
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Image
+            source="../images/accept.png"
+            style={styles.button}
+            resizeMode="stretch"
+          />
+        </TouchableOpacity>
+      </SafeAreaView>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
+
+const ProfileOverview = () => {
+  return (
+    <SafeAreaView style={matchStyles.match}>
+      <Text>Hello World</Text>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -22,24 +53,39 @@ const styles = StyleSheet.create({
     backgroundColor: '#fffdd3',
     alignItems: 'center',
     justifyContent: 'center',
-    fontWeight: 'bold',
+  },
+  topMenu: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  matchMenu: {
+    flex: 5,
+    backgroundColor: 'green',
+  },
+  bottomMenu: {
+    flex: 1,
+    backgroundColor: 'blue',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   title: {
     color: 'black',
     fontSize: 30,
     fontWeight: 'bold',
   },
-  input: {
-    height: 40,
-    width: '70%',
-    margin: 1,
-    borderWidth: 1,
-    padding: 10,
+  button: {
+    color: 'white',
+    margin: 10,
   },
-  buttonText: {
-    textAlign: 'center',
-    color: 'black',
-    fontSize: 12,
-    padding: 5,
+});
+
+const matchStyles = StyleSheet.create({
+  match: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
