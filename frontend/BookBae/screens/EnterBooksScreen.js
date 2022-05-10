54b76d0b8b860fc08f6f6b434cb450db9dce2c85
Elliott Zackrone <ezackr@cs.washington.edu>
2022-05-10 14:04:57 -0700

    added packages to transformIgnorePatterns in package.json

--- frontend/BookBae/screens/EnterBooksScreen.js
+++ frontend/BookBae/screens/EnterBooksScreen.js
@@ -1,40 +1,49 @@
 import React from 'react';
-import { SafeAreaView, View, StyleSheet, Text, TextInput, Pressable, Image } from 'react-native';
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  Pressable,
+  Image,
+} from 'react-native';
 
 const EnterBooksScreen = ({navigation}) => {
+  var bookCover =
+    'https://www.pngitem.com/pimgs/m/19-191625_icon-plus-png-gray-plus-icon-png-transparent.png';
 
-    var bookCover = 'https://www.pngitem.com/pimgs/m/19-191625_icon-plus-png-gray-plus-icon-png-transparent.png'
+  const [text] = React.useState(null);
 
-    const [text] = React.useState(null);
+  //start suggesting book title options from text input
+  //change bookCover to chosen book
+  const onChangeText = () => {};
 
-    //start suggesting book title options from text input
-    //change bookCover to chosen book
-    const onChangeText = () => {
+  //we probably don't need to do anything here, we can store books as they are added in onChangeText
+  const onPress = () => {
+    console.log(bookCover);
+    navigation.navigate('EnterPhotoScreen');
+  };
 
-    }
-
-    //we probably don't need to do anything here, we can store books as they are added in onChangeText
-    const onPress = () => {
-        console.log(bookCover)
-        navigation.navigate('EnterPhotoScreen')
-    }
-
-    return (
-        <SafeAreaView style={styles.container}>
-            <Text style={styles.title}>Start Typing a Book Title:</Text>
-            <TextInput style={styles.input} multiline={true} onChangeText={onChangeText} value={text} placeholder="xxxxx"/>
-            <View style={styles.imageContainer}>
-                <Image style={styles.image} source={{uri:bookCover}}/>
-            </View>
-            <Pressable
-                style={styles.button}
-                onPress={onPress}>
-                <Text style={styles.buttonText}>Next</Text>
-             </Pressable>
-        </SafeAreaView>
-    );
-
-}
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Start Typing a Book Title:</Text>
+      <TextInput
+        style={styles.input}
+        multiline={true}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="xxxxx"
+      />
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{uri: bookCover}} />
+      </View>
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonText}>Next</Text>
+      </Pressable>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   title: {
@@ -65,22 +74,22 @@ const styles = StyleSheet.create({
     height: 200,
   },
   button: {
-      alignItems: 'center',
-      justifyContent: 'center',
-      paddingVertical: 12,
-      paddingHorizontal: 32,
-      borderRadius: 4,
-      elevation: 3,
-      backgroundColor: '#BD2A2A',
-      marginTop: 5,
-      marginBottom: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: '#BD2A2A',
+    marginTop: 5,
+    marginBottom: 5,
   },
   buttonText: {
-      fontSize: 16,
-      lineHeight: 21,
-      fontWeight: 'bold',
-      letterSpacing: 0.25,
-      color: 'white',
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
   },
 });
 
