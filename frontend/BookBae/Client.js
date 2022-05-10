fb2490cf27e111e24175c9e60e468772d29d81f4
Helena Stafford <helena98@cs.washington.edu>
2022-05-09 20:50:58 -0700

    Add remaining methods and documentation

--- frontend/BookBae/Client.js
+++ frontend/BookBae/Client.js
@@ -1,4 +1,4 @@
-// Utility class for making requests to server
+/** Utility class for making requests to server */
 class Client {
   static IP = '127.0.0.1';
   static PORT = '8080';
@@ -9,9 +9,17 @@ class Client {
 
   static authToken; // Probably not the right place to store this
 
+  /**
+  * Gets the user info given the user's authToken
+  * @return {{email, name, preferredGender, gender,
+  *          favGenre, birthday, bio, zipcode}}
+  */
   static async getUserInfo() {
-    return fetch(Client.ROOT_PATH + 'user', {
+    return await fetch(Client.ROOT_PATH + 'user', {
       method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + Client.authToken
+      },
     })
       .then(response => response.json())
       .catch(error => {
@@ -19,13 +27,21 @@ class Client {
       });
   }
 
-  static async setUserInfo(loginInfo) {
+  /**
+  * Updates user info
+  * @param {{email, name, preferredGender, gender,
+  *          favGenre, birthday, bio, zipcode}} userInfo
+  * @return {{email, name, preferredGender, gender,
+  *          favGenre, birthday, bio, zipcode}} the server's (new) user info
+  */
+  static async setUserInfo(userInfo) {
     return await fetch(Client.ROOT_PATH + 'user', {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + Client.authToken
       },
-      body: JSON.stringify(loginInfo),
+      body: JSON.stringify(userInfo),
     })
       .then(response => response.json())
       .catch(error => {
@@ -33,13 +49,19 @@ class Client {
       });
   }
 
-  static async createUser(accountInfo) {
+  /**
+  * Creates a new user account
+  * @param {string} email - the email to associate new account with
+  * @param {string} password - the password that the user will use to access account
+  */
+  static async createUser(email, password) {
+    const credentials = { email, password }
     return await fetch(Client.ROOT_PATH + 'create', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(accountInfo),
+      body: JSON.stringify(credentials),
     })
       .then(response => response.status)
       .catch(error => {
@@ -47,16 +69,77 @@ class Client {
       });
   }
 
-  static async logIn(credentials) {
-    this.authToken = await fetch(Client.ROOT_PATH + 'login', {
+  /**
+  * Attempts to log in to an account with the given email and password
+  * @param {string} email - the email associated with the account
+  * @param {string} password - the password associated with the account
+  */
+  static async logIn(email, password) {
+    const credentials = { email, password }
+    Client.authToken = await fetch(Client.ROOT_PATH + 'login', {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(credentials),
     })
       .then(response => response.json())
-      .catch(() => {});
+      .catch(error => {
+        console.error('Failed to log in', error)
+      });
+  }
+
+  /**
+  * Gets a list of recommended potential matches
+  * @return {[{userid, name, preferredGender, gender, favGenre, birthday, bio}]}
+  */
+  static async getPotentialMatches() {
+    const potentialMatches = await fetch(Client.ROOT_PATH + 'recommends', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + Client.authToken
+      },
+    })
+      .then(response => response.json())
+      .catch(error => {
+        console.error('Failed to get potential matches', error)
+      });
+  }
+
+  /**
+  * Likes user with given id
+  * @param {string} userid - the user to like
+  */
+  static async sendLike(userid) {
+    await fetch(Client.ROOT_PATH + 'like', {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + Client.authToken
+      },
+      body: JSON.stringify({userid}),
+    })
+      .then(response => response.json())
+      .catch(error => {
+        console.error('Failed to send like', error)
+      });
+  }
+
+  /**
+  * Gets existing matches for chat
+  * @return {{displayName, photoUrl, lastMessage, likeId}}
+  */
+  static async getChats() {
+    const chats = await fetch(Client.ROOT_PATH + 'chats', {
+      method: 'GET',
+      headers: {
+        'Authorization': 'Bearer ' + Client.authToken
+      },
+    })
+      .then(response => response.json())
+      .catch(error => {
+        console.error('Failed to get chats')
+      });
   }
 }
 
