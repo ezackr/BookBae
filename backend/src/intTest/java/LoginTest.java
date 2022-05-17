18ddc0abddd0e3a6b684febcc884335ae6715796
Joshua Peterson <jpete4@uw.edu>
2022-05-17 14:11:39 -0700

    first step for int test

--- backend/src/intTest/java/LoginTest.java
+++ backend/src/intTest/java/LoginTest.java
@@ -1,10 +1,32 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import java.io.File;
+import org.glassfish.embeddable.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
+    private static Deployer deployer;
+    private static File archive;
+    @BeforeAll
+    static void initGlassfish() throws GlassFishException {
+        var props = new GlassFishProperties();
+        props.setPort("http-listener", 8008);
+        var glassfish = GlassFishRuntime.bootstrap().newGlassFish(props);
+        glassfish.start();
+        deployer = glassfish.getDeployer();
+        archive = new File(System.getProperty("bookbae.archive"));
+        deployer.deploy(archive, "--contextroot=api", "--name=api");
+    }
+
     @Test
-    void helloIntegrationTest() {
-        assertEquals("hello", "hello");
+    void testGlassfish() {
+        assertEquals("a", "a");
+    }
+
+    @AfterAll
+    static void teardownGlassfish() throws GlassFishException {
+        deployer.undeploy("api");
     }
 }
