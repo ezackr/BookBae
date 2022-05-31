ef8a62c86bb9fa2300c478d89209cdc056dc5e02
Elliott Zackrone <97812144+ezackr@users.noreply.github.com>
2022-05-30 17:12:57 -0700

    adjusted how features are loaded to add default null handling

--- frontend/BookBae/screens/HomeScreen.js
+++ frontend/BookBae/screens/HomeScreen.js
@@ -8,22 +8,74 @@ import {
 } from 'react-native';
 import Client from '../Client.js';
 
+/**
+ * Main App screen used to suggest matches to users.
+ */
 const HomeScreen = ({navigation}) => {
-  const [rerender, setRerender] = React.useState(false);
-  const outOfMatches = {key: 'empty', name: 'no more matches'};
-  const [matches, setMatches] = React.useState([outOfMatches]);
+  const [rerender, setRerender] = React.useState(false); // used to force screen to rerender.
+  // default message if user is out of matches.
+  const outOfMatches = {
+    key: 'empty',
+    userId: 'empty',
+    name: 'No Matches Found',
+    age: '',
+    gender: '',
+    bio: 'Try re-loading the screen or come again later!',
+    favGenre: '',
+  };
+  const [matches, setMatches] = React.useState([outOfMatches]); // list of matches.
 
+  // loads list of potential matches when entering this screen.
   React.useEffect(() => {
+    // calls method from Client.js to get list of matches from database.
     Client.getPotentialMatches().then(data => {
+      // sets the default empty message at the front of the array.
       setMatches([outOfMatches]);
+
+      // collects/organizes user info for all potential matches.
       for (let i = 0; i < data.length; i++) {
         let match = data[i];
-        if ('name' in match) {
+        console.log(match);
+        // does not add user if they do not have a 'name' or 'userId'.
+        if ('name' in match && 'userId' in match) {
+          // set default profile parameters.
+          let matchInfo = {
+            key: i,
+            userId: match.userId,
+            name: match.name,
+            age: 'N/A',
+            gender: '',
+            bio: match.name + ' does not have a bio yet.',
+            favGenre: 'N/A',
+          };
+
+          // add user profile parameters if possible.
+          // adds age.
+          if ('birthday' in match) {
+            // calculates user age using their birthday.
+            let birthYear = parseInt(match.birthday.split('-'));
+            if (!isNaN(birthYear)) {
+              let currentYear = new Date().getFullYear();
+              let age = currentYear - birthYear;
+              matchInfo.age = age;
+            }
+          }
+          // adds gender.
+          if ('gender' in match) {
+            matchInfo.gender = match.gender;
+          }
+          // adds bio.
+          if ('bio' in match) {
+            matchInfo.bio = match.bio;
+          }
+          // adds favorite genre.
+          if ('favGenre' in match) {
+            matchInfo.favGenre = match.favGenre;
+          }
+
+          // add new user data to the end of the array.
           setMatches(prevState => {
-            prevState.push({
-              key: i + 1,
-              name: match.name,
-            });
+            prevState.push(matchInfo);
             return [...prevState];
           });
         }
@@ -83,7 +135,7 @@ const ProfileCard = ({profile}) => {
           source={require('../images/title2.jpeg')}
         />
       </SafeAreaView>
-      <Text style={matchStyles.frontName}>{profile.name}</Text>
+      <Text style={matchStyles.frontName}>{profile.name} {profile.age}{profile.gender}</Text>
       <SafeAreaView style={matchStyles.bookDisplay}>
         <Image
           style={matchStyles.book}
