19ac00e93e28594bd252c7cd1ad3f6a9dac79e2e
Helena Stafford <helena98@cs.washington.edu>
2022-06-01 16:06:25 -0700

    Improve book selection

--- frontend/BookBae/screens/onboarding/EnterBooksScreen.js
+++ frontend/BookBae/screens/onboarding/EnterBooksScreen.js
@@ -7,67 +7,100 @@ import {
   TextInput,
   Pressable,
   FlatList,
+  Image
 } from 'react-native';
 import axios from 'axios';
 
 const EnterBooksScreen = ({route, navigation}) => {
   const [text, setText] = React.useState(null);
   const [bookList, setBookList] = React.useState([]);
-  const [idList, setIdList] = React.useState([]);
+  const [searchResults, setSearchResults] = React.useState([]);
 
-  // we probably don't need to do anything here, we can store books as they are added in onChangeText
+  // Pass the ids of the selected books on to the next onboarding screen
   const onPress = () => {
+    console.log('leave books');
+    const idList = bookList.map(book => book.id);
     navigation.navigate('EnterPhotoScreen', {
       ...route.params,
       books: idList,
     });
   };
 
-  const addNewBook = () => {
+  // Adds a book to the user's list
+  const addNewBook = (book) => {
+    setBookList(prevState => {
+      prevState.push(book);
+      return [...prevState];
+    })
+  };
+
+  // Displays search results;
+  const onSearch = () => {
     axios
       .get('https://www.googleapis.com/books/v1/volumes?q=' + text)
-      .then(data => {
-        let book = data.data.items[0];
-        // add new book to stored list.
-        setBookList(prevState => {
-          prevState.push({
-            title: book.volumeInfo.title,
-          });
-          return [...prevState];
-        });
-        setIdList(prevState => {
-          prevState.push(book.id);
-          return [...prevState];
-        });
-        console.log(idList);
-        console.log(bookList);
+      .then(response => {
+        console.log(response.data.items[0].volumeInfo.imageLinks);
+        setSearchResults(response.data.items);
       });
-    setText('');
   };
+
 
   return (
     <SafeAreaView style={styles.container}>
+
       <Text style={styles.title}>Start Typing a Book Title:</Text>
+
+      {/* search bar */}
       <TextInput
         style={styles.input}
         placeholder="Insert Book Title"
         onChangeText={setText}
-        onSubmitEditing={() => addNewBook()}
+        onSubmitEditing={onSearch}
         defaultValue={text}
       />
+
+      {/* search button */}
+      <Pressable style={styles.button} onPress={onSearch}>
+        <Text style={styles.buttonText}>Search</Text>
+      </Pressable>
+
+      {/* search results */}
+      <View style={styles.bookList}>
+        <FlatList
+          data={searchResults}
+          extraData={searchResults}
+          renderItem={({item}) => <BookListItem book={item} onClick={() => addNewBook(item)}/>}
+        />
+      </View>
+
+      <Text style={styles.title}>My Bookshelf:</Text>
+      {/* selected books */}
       <View style={styles.bookList}>
         <FlatList
           data={bookList}
           extraData={bookList}
-          renderItem={({item}) => <Text style={styles.book}>{item.title}</Text>}
+          renderItem={({item}) => <BookListItem book={item} />}
         />
       </View>
+
+      {/* next page button */}
       <Pressable style={styles.button} onPress={onPress}>
         <Text style={styles.buttonText}>Next</Text>
       </Pressable>
     </SafeAreaView>
   );
 };
+
+const BookListItem = ({book, onClick}) => {
+  return (
+    <Pressable onPress={onClick}>
+        <View style={{flexDirection: 'row'}}>
+          <Image style={styles.image} source={{uri: book.volumeInfo.imageLinks.smallThumbnail}}/>
+          <Text style = {styles.book}> {book.volumeInfo.title} </Text>
+        </View>
+    </Pressable>
+  )
+}
 
 const styles = StyleSheet.create({
   title: {
@@ -81,7 +114,7 @@ const styles = StyleSheet.create({
   },
   book: {
     color: 'black',
-    fontSize: 24,
+    fontSize: 20,
   },
   input: {
     height: 40,
@@ -102,8 +135,8 @@ const styles = StyleSheet.create({
     paddingBottom: 5,
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 40,
+    height: 60,
   },
   button: {
     alignItems: 'center',
